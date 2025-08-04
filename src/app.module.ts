fcce942f28687ae3310285965f77f98ab18ee303
Anil Kumar <mak97047@gmail.com>
2025-08-04 18:11:11 +0530

    add a products module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,13 +6,15 @@ import dataSource from './config/typeorm-config';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { ProductsModule } from './src/modules/products/products.module';
+import { ProductsModule } from './modules/products/products.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal:true
   }),
     TypeOrmModule.forRoot({ ...dataSource.options }), 
-    UsersModule, AuthModule
+    UsersModule, AuthModule, ProductsModule
   ], 
   controllers: [AppController],
   providers: [AppService],
